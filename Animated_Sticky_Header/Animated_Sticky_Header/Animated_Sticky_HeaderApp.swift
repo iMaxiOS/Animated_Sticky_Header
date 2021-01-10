f6b70999cb2160ced9d44aeb9ddc0986a342540d
Macostik <y.granchenko@gmail.com>
2021-01-10 11:30:43 +0200

    create project

---
+++ Animated_Sticky_Header/Animated_Sticky_Header/Animated_Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Sticky_HeaderApp.swift
+//  Animated_Sticky_Header
+//
+//  Created by Maxim Granchenko on 10.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
